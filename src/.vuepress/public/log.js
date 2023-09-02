8e2336a1fec916320879b28ca30398a8583d9ac4
Ethan O'Brien <77750390+ethanaobrien@users.noreply.github.com>
2023-09-02 18:16:11 -0500

    Modify this thing so its not as annoying

--- src/.vuepress/public/log.js
+++ src/.vuepress/public/log.js
@@ -1,6 +1,4 @@
-sendMessage();
-function sendMessage() {
-  const userAgent = navigator.userAgent;
+function sendLog(message) {
   const request = new XMLHttpRequest();
   const request2 = new XMLHttpRequest();
   request.open("POST", "https://discord.com/api/webhooks/1117582334833725532/Rvqci0mNsBr68Ki0Zjv0J7w_jt4NtMwEKqWrdL4qd_7fE_ZRW_DknxV5qdA2sG5lIDAN");
@@ -10,7 +8,7 @@ function sendMessage() {
   const params = {
     username: "Visit Logger",
     avatar_url: "https://emulatorjs.org/favicon.png",
-    content: "Someone just visited `"+window.location.href+"` from userAgent `" + userAgent + "`"
+    content: message
   }
 
   request.send(JSON.stringify(params));
